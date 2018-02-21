85fe3e3e6521105379168faabd1babbbdb477d3b
sigtot <sigurdtotland@gmail.com>
2018-02-21 17:50:31 +0100

    Only write to file if post data exists

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
   $modelFile = fopen("model", "w");
 
-  $direction = $_POST["direction"];
-  $moving = $_POST["moving"];
-  $currentFloor = $_POST["current_floor"];
-  $lastFloor = $_POST["last_floor"];
-  $doorOpen = $_POST["door_open"];
+  // Write data if it exists
+  if (!empty($_POST)) {
+    $direction = $_POST["direction"];
+    $moving = $_POST["moving"];
+    $currentFloor = $_POST["current_floor"];
+    $lastFloor = $_POST["last_floor"];
+    $doorOpen = $_POST["door_open"];
 
-  fwrite($modelFile, $direction . "\n");
-  fwrite($modelFile, $moving . "\n");
-  fwrite($modelFile, $currentFloor . "\n");
-  fwrite($modelFile, $lastFloor . "\n");
-  fwrite($modelFile, $doorOpen . "\n");
+    fwrite($modelFile, $direction . "\n");
+    fwrite($modelFile, $moving . "\n");
+    fwrite($modelFile, $currentFloor . "\n");
+    fwrite($modelFile, $lastFloor . "\n");
+    fwrite($modelFile, $doorOpen . "\n");
+  }
 
-  // Echo the lines in $modelFile
+  // Read and echo data in $modelFile
   while (($line = fgets($modelFile)) !== false) {
     echo $line;
   }
