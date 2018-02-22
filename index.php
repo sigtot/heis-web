a1c96ec5dec662bc353fa8882216bfe103a6a43b
sigtot <sigurdtotland@gmail.com>
2018-02-22 13:22:50 +0100

    Recieve orders

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <?php
-  header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 
-  if (!empty($_POST)) {
-    // Write data if it exists
-    $modelFile = fopen("model", "w");
-    $direction = $_POST["direction"];
-    $moving = $_POST["moving"];
-    $currentFloor = $_POST["current_floor"];
-    $lastFloor = $_POST["last_floor"];
-    $doorOpen = $_POST["door_open"];
+$modelFileName = "model";
 
-    fwrite($modelFile, "direction" . "," . $direction . "\n");
-    fwrite($modelFile, "moving" . "," . $moving . "\n");
-    fwrite($modelFile, "current_floor" . "," . $currentFloor . "\n");
-    fwrite($modelFile, "last_floor" . "," . $lastFloor . "\n");
-    fwrite($modelFile, "door_open" . "," . $doorOpen . "\n");
+if (!empty($_POST)) {
+  // Write data if it exists
+  $modelFile = fopen($modelFileName, "w");
+  $direction = $_POST["direction"];
+  $moving = $_POST["moving"];
+  $currentFloor = $_POST["current_floor"];
+  $lastFloor = $_POST["last_floor"];
+  $doorOpen = $_POST["door_open"];
 
-    fclose($modelFile);
-  } else {
-    // Read and echo data
-    $data = [];
-    $modelFile = fopen("model", "r");
-    while (($line = fgets($modelFile)) !== false) {
-      $data[explode(",", $line)[0]] = intval(explode(",", $line)[1]);
-    }
-    echo json_encode($data);
-    fclose($modelFile);
+  fwrite($modelFile, "direction" . "," . $direction . "\n");
+  fwrite($modelFile, "moving" . "," . $moving . "\n");
+  fwrite($modelFile, "current_floor" . "," . $currentFloor . "\n");
+  fwrite($modelFile, "last_floor" . "," . $lastFloor . "\n");
+  fwrite($modelFile, "door_open" . "," . $doorOpen . "\n");
+
+  fclose($modelFile);
+} else {
+  // Read and echo data
+  $data = [];
+  $modelFile = fopen($modelFileName, "r");
+  while (($line = fgets($modelFile)) !== false) {
+    $data[explode(",", $line)[0]] = intval(explode(",", $line)[1]);
   }
+  echo json_encode($data);
+  fclose($modelFile);
+}
---
+++ orderReciever.php
@@ -0,0 +1,15 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+$ordersFileName = "orders";
+if (!empty($_POST)) {
+  // Write data if it exists
+  $ordersFile = fopen($ordersFileName, "w");
+  $orders = $_POST["orders"];
+  foreach ($orders as $order) {
+    fwrite($ordersFile, $order);
+  }
+  fclose($ordersFile);
+} else {
+  // Read and echo data
+}
+
